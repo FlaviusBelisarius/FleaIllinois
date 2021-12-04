ec4147cf188c2e631e5329f67a1c5ccf048a1b56
Tiancheng Jiang <tj15@illinois.edu>
2021-12-04 21:05:49 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,8 @@ import './App.css'
 import Login from './login/Login'
 import User from './user/User'
 import Details from './details/Details'
-import Post from './../../post/Post'
+import Gallery from './gallery/Gallery.jsx'
+import Post from './post/Post'
 
 function App() {
   return (
@@ -15,8 +16,13 @@ function App() {
       <BrowserRouter>
 
         <Routes>
-          <Route exact path='/login' element={<Login/>} />
+          <Route exact path='/' element={<Gallery/>} />
         </Routes>
+
+        <Routes>
+          <Route path='/login' element={<Login/>} />
+        </Routes>
+
         
         <Routes>
           <Route path='/user' element={<User/>} />
