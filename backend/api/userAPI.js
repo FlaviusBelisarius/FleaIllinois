38cf25ef5170a276bbe65c7bd6d6643bf7242ee2
Jerry Nie <707744613nrp@gmail.com>
2021-12-06 00:36:51 -0800

    another version of user API

--- backend/api/userAPI.js
+++ backend/api/userAPI.js
@@ -1,191 +1,196 @@
-var mongoose = require('mongoose');
-var Product = require('../models/user');
 var express = require('express');
 router = express.Router();
 
-var emailSet = new Set();  // check for duplicate emails
+var User = require('../models/user');
+var Product = require('../models/product');
 
-var userdata = mongoose.model('users', UserSchema);
-
-userdata.find().exec().then(ret => {
-    for (var i = 0; i < ret.length; i++){
-        emailSet.add(ret[i].email)
+function parseQuery(condition) {
+    if (typeof condition !== 'undefined'){
+        return JSON.parse(condition);
     }
-})
+    return condition;
+}
 
-app.post('/api/users', function(req, res){
-    var data = req.body;
-    if (!data.name || !data.email){
-        res.status(404).send({
-            "message": "Error!",
-            "data": "need name & email"
-        })
-    }
-    if (emailSet.has(data.email)){
-        res.status(404).send({
-            "message": "Error!",
-            "data": "duplicate email:" + data.email
-        })
-    }
-    
-    try{
-        mongoose.connection.collection('users').insert(data);
-    }catch (error) {
-        res.status(500).send({
-            "message": "Error!",
-            "data": error
-        });
-    }
-
-    emailSet.add(data.email)
-
-    const cont = {
-        "message": "ok",
-        "data": data
-    }
-    res.status(201).send(cont);
-})
-
-app.get('/api/users', function(req, res){
-
-    var result = userdata.find()
-    
-    if (req.query.where){
-        result = result.where(JSON.parse(req.query.where))
-    }
-    
-
-    if (req.query.select){
-        result = result.select(JSON.parse(req.query.select))
-    }
-
-    if (req.query.sort){
-        result = result.sort(JSON.parse(req.query.sort))
-    }
-
-    if (req.query.limit){
-        var limit = parseInt(req.query.limit, 10)
-        result = result.limit(limit);
-    }
-
-    if (req.query.skip){
-        result = result.skip(parseInt(req.query.skip, 10));
-    }
-
-    if (req.query.count && req.query.count == "true"){
-        result.countDocuments().exec().then(ret => {
-            const cont = {
-                "message": "ok",
-                "data": ret
+router.get('/', function(req, res) {
+    User.find(parseQuery(req.query.where))
+        .sort(parseQuery(req.query.sort))
+        .select(parseQuery(req.query.select))
+        .skip(parseQuery(req.query.skip))
+        .limit(parseQuery(req.query.limit))
+        .exec()
+        .then(users => {
+            if(users){
+                if(req.query.count){
+                    return res.status(200).json({
+                        message: "OK", 
+                        data: users.length
+                    });
+                }
+                if(users.length == 0){
+                    return res.status(404).json({
+                        message: "User not found",
+                        data: []
+                    });
+                }
+                return res.status(200).json({
+                    message: "OK", 
+                    data: users
+                });
             }
-            res.status(200).send(cont);
-        }).catch(err => {
-            res.status(404).send({
-                "message": "Error!",
-                "data": err
-            })
-        })
-    }else{
-        result.exec().then(ret => {
-            const cont = {
-                "message": "ok",
-                "data": ret
-            }           
-            res.status(200).send(cont);
-        }).catch(err => {
-            res.status(404).send({
-                "message": "Error!",
-                "data": err
-            })
-        })
-    }
-})
 
-app.get('/api/users/:id', function (req, res) {
-    var ret;
-    try{
-        ret = userdata.find({"_id": req.params.id})
-        if (req.query.select){
-            ret = ret.select(JSON.parse(req.query.select))
+        })
+        .catch(err => {
+            return res.status(500).json({
+                message: "Server Error",
+                data: err
+            });
+        })
+});
+
+router.get('/:id', function(req, res) {
+    User.findById(req.params.id, function(err, targetUser){
+        if(targetUser){
+            return res.status(200).json({
+                message: "OK",
+                data: targetUser
+            });
+        }else if(err){
+            if (err.name == "CastError") {
+                return res.status(400).json({
+                    message:"User id is invalid", 
+                    data: {"Invalid id is": req.params.id}
+                });
+            }
+            return res.status(500).json({
+                message: "Server error",
+                data: err
+            });
+        }else{
+            return res.status(404).json({
+                message: "User not found",
+                data: []
+            });
         }
-    }catch(error){
-        res.status(404).send({
-            "message": "Error!",
-            "data": error
-        });
-    }
-    ret.exec().then(result => {
-        res.status(200).send({
-            "message": "ok",
-            "data": result
-        });
-    }).catch(err => {
-        res.status(500).send({
-            "message": "Error!",
-            "data": err
-        })
-    })
-})
+    });
+});
 
-app.put('/api/users/:id', function (req, res) {
-    var data = req.body;
-    if (!data.name || !data.email){
-        res.status(404).send({
-            "message": "Error!",
-            "data": "need name & email"
-        })
-    }
-    var currEmail;
-    
-    userdata.findById(req.params.id).exec().then(ret => {
-        emailSet.delete(ret.email);
-        currEmail = ret.email;
-    }).catch(err =>{
-        res.status(404).send({
-            "message": "Error!",
-            "data": err
-        });
-    })
-    if (emailSet.has(data.email)){
-        res.status(404).send({
-            "message": "Error!",
-            "data": "duplicate email"
-        })
-    }
-    userdata.replaceOne({"_id": req.params.id}, req.body).exec().then(ret =>{
-        res.status(200).send({
-            "message": "ok",
-            "data": ret
-        });
-    }).catch(err =>{
-        emailSet.add(currEmail);   // add back to emailset
-        res.status(500).send({
-            "message": "Error!",
-            "data": err
-        });
-    })
-})
+function validateEmail(email){ // make sure input email is a valid email
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
 
-app.delete('/api/users/:id', function (req, res) {
-    var currEmail;
-    userdata.findById(req.params.id).exec().then(ret => {
-        currEmail = ret.email;
-    }).catch(err =>{
-        res.status(404).send({
-            "message": "Error!",
-            "data": err
+router.post('/', function(req, res) {
+    if (!req.body.email) {
+        return res.status(400).json({
+            message:'Post request must have user email', 
+            data: []
         });
-    })
-    userdata.deleteOne({"_id": req.params.id}).exec().then(ret =>{
-        emailSet.delete(currEmail);
-        res.status(200).send({
-            "message": "ok",
-            "data": ret
+    }
+    if(!validateEmail(req.body.email)){
+        return res.status(400).json({
+            message:"user email is invalid", 
+            data: {"Invalid email is": req.body.email}
         });
-    }).catch(err =>{
-        res.status(404).send({
-            "message": "Error!",
-            "data": err
-        });
-    })
-})
+    }
+    const newUser = {
+        email: req.body.email, // must have
+        name: req.body.name, // it's ok to be null or empty
+        verified: req.body.verified,
+    }
+    User.findById(req.body.email, function(userErr, targetUser){
+        if(targetUser){
+            return res.status(404).json({
+                message: "User already exists",
+                data: []
+            });
+        }else if(userErr){
+            if (userErr.name == "CastError") {
+                return res.status(400).json({
+                    message:"user email is invalid", 
+                    data: {"Invalid email is": req.body.email}
+                });
+            }
+            return res.status(500).json({
+                message: "Server error",
+                data: err
+            });
+        }else{
+            User.create(newUser)
+                .then(postRes => {
+                    return res.status(201).json({
+                        message: "User successfully created", 
+                        data: postRes
+                    })
+                })
+                .catch(err => {
+                    return res.status(500).json({
+                        message: "Server Error",
+                        data: err
+                    });
+                });
+        }
+    });
+});
+
+router.put('/:id', function(req, res) {
+    if(req.body.name){
+        Product.updateMany({sellerID: req.params.id}, {"$set":{"sellerName": req.body.name}});
+        Product.updateMany({buyerID: req.params.id}, {"$set":{"buyerName": req.body.name}});
+    }
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, updatedUser){
+        if(updatedUser){
+            return res.status(201).json({
+                message: 'User updated',
+                data: updatedUser
+            });
+        }else if(err){
+            if (err.name == "CastError") {
+                return res.status(400).json({
+                    message:"User id is invalid", 
+                    data: {"Invalid id is": req.params.id}
+                });
+            }
+            return res.status(500).json({
+                message: "Server error",
+                data: err
+            });
+        }else{
+            return res.status(404).json({
+                message: "User not found",
+                data: []
+            });
+        }
+    });
+});
+
+router.delete('/:id', function(req, res) {
+    User.findByIdAndDelete(req.params.id, function (err, targetUser){
+        if(targetUser){
+            return res.status(200).json({
+                message: 'User deleted',
+                data: targetUser
+            });
+        }else if(err){
+            if (err.name == "CastError") {
+                return res.status(400).json({
+                    message:"User id is invalid", 
+                    data: {"Invalid id is": req.params.id}
+                });
+            }
+            return res.status(500).json({
+                message: "Server error",
+                data: err
+            });
+        }else{
+            return res.status(404).json({
+                message: "User not found",
+                data: []
+            });
+        }
+    });
+});
+
+module.exports = router;
