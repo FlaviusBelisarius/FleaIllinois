9367ab7d03268c205542069fa0cdfe6394145cef
Zewei Long <zeweilong@host-208-125.ilcuoct.champaign.il.us.clients.pavlovmedia.net>
2021-12-11 21:18:00 -0600

    search bar final version

--- frontend/src/common/Header.js
+++ frontend/src/common/Header.js
@@ -46,36 +46,3 @@ const Header = () => {
 }
 
 export default Header
-
-
-// const Header = () => {
-//     const { currentUser, logoutUser } = useAuth()
-//     console.log("user: ", currentUser)
-
-//     const handleLogout = (e) => {
-//         e.preventDefault()
-//         logoutUser()
-//     }
-
-//     return (
-//         <header className='header'>
-//             <Link to='/' className='link-heading'><h1>Flea Illinois</h1></Link>
-//             <form className='form-search-bar'>
-//                 <input type="text" id='input-search-bar'/>
-//                 <input type="submit" value="Search" />
-//             </form>
-//             <div className="container-headings">
-//                 {currentUser 
-//                     ? <Fragment>
-//                         <Link to='/post' className='link-heading'>Post Product</Link>
-//                         <Link to='/user' className='link-heading'>My account</Link>
-//                         <Link to='' className='link-heading' onClick={handleLogout}>Logout</Link>
-//                     </Fragment>
-//                     : <Link to='/login' className='link-heading'>Login</Link>
-//                 }
-//             </div>
-//         </header>
-//     )
-// }
-
-// export default Header
