1d7d301564c0dbb0db1531298b4be616b04f519a
Wenshan Xiong <wenshanxiong@Wenshans-MacBook-Air.local>
2021-12-15 03:02:49 -0600

    fixed style

--- frontend/src/user/ProductListItem.js
+++ frontend/src/user/ProductListItem.js
@@ -6,8 +6,8 @@ const ProductListItem = ({ product }) => {
         <div className="product-list-item">
             <img className="img-product" src={product.productImage} alt="" />
             <div className="product-detail">
-                <p>{product.productName}</p>
-                <Link to={`/details/${product._id}`}>View Detail</Link>                            
+                <h2 className='h2-user-product'>{product.productName}</h2>
+                <Link to={`/details/${product._id}`} className='link-detail'>View Detail</Link>                            
             </div>
             <p className="product-date">{product.dateCreated}</p>
         </div>
